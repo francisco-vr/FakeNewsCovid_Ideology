7c4e99363c8bd1e368e82ac8e33aa721220b8a3b
gnusuri <gnusuri@debian-BULLSEYE-live-builder-AMD64>
2023-06-07 14:08:10 -0400

    update scripts

--- Scripts/5.Matching.R
+++ Scripts/5.Matching.R
@@ -9,8 +9,13 @@ covid<-dplyr::filter(covid, Sex=="Female" | Sex=="Male")
 covid$Sex <-relevel(factor(covid$Sex), ref = "Female")
 covid$Sex <-as.factor(covid$Sex)
 
+##filter non-partisan in Extreme-moderate
+
+covid <-covid%>%
+  dplyr::filter(ExtMod!="Ninguno")
 
 
+### Testing with partisan/non-partisan
 ## Matching using ideology, age & gender
 
 m.out0 <- matchit(as.factor(IdePol) ~ EcoCount + Citizen + Age + Sex + NivEco, method =NULL, data = covid,
@@ -23,15 +28,6 @@ dim(dta_m)
 table(dta_m$Sex)
 
 
-## testing with nearest neighbor
-
-m.out1 <- matchit(as.factor(IdePol) ~ EcoCount + Citizen + Age + Sex + NivEco, method ="nearest", data = covid,
-                  distance = "glm", link = "logit")
-
-summary(m.out1, un=FALSE)
-plot(m.out1, type= "jitter", interactive = FALSE)
-plot(m.out1, type = "density", interactive = FALSE, which.xs = ~ EcoCount + Citizen + Age + Sex + NivEco)
-
 ## poor performance of nearest neihbor. now we test with full method
 
 m.out2 <- matchit(as.factor(IdePol) ~ EcoCount + Citizen + Age + Sex + NivEco, method ="full", data = covid,
@@ -42,62 +38,13 @@ m.out2
 summary(m.out2, un=FALSE)
 plot(summary(m.out2))
 
-##looks very good. we will test coarced matching btw
-
-m.out3 <- matchit(as.factor(IdePol) ~ EcoCount + Citizen + Age + Sex + NivEco, method = "cem", data = covid,
-                  distance = "glm", link = "logit")
-m.out3
-
-summary(m.out3, un=FALSE)  #even better than full !
-plot(summary(m.out3))
-
-m.data_cem <-match.data(m.out3)
-head(m.data_cem)
-
-logit_cem <-glm(Con ~ IdePol * (EcoCount + Citizen + Age + Sex + NivEco),
-                data = m.data_cem,family = "binomial"(link = "logit") )
-summary(logit_cem)
-
-avg_comparisons(logit_cem,
-                variables = "IdePol",
-                vcov = ~subclass,
-                newdata = subset(m.data_cem, IdePol == 1),
-                wts = "weights")
-
-####
-
-m.data <-match.data(m.out2)
-head(m.data)
-
-
-logit_nearest <-glm(Con ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
-                 data = m.data, family = "binomial"(link = "logit"))
-
-summary(logit_nearest)
-
-sjPlot::plot_model(logit_nearest)
-
-
-table(covid$Sex)
-
 
 ######### Ok the fake news about Contraloria didn't work to much. lets try covid
 
-## testing regression with nearest neibor
-
-logit_covid_nearest <-glm(COVID ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
-                    data = m.data, family = "binomial"(link = "logit"))
-summary(logit_covid_nearest)
-
-avg_comparisons(logit_covid_nearest,
-                variables = "IdePol",
-                vcov = ~subclass,
-                newdata = subset(m.data, IdePol == 1),
-                wts = "weights")
 
 ## didn't work. lets try with full matching
 
-logit_covid_full <-glm(COVID ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
+logit_covid_full <-glm(Con ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
                           data = m.data_full, family = "binomial"(link = "logit"))
 summary(logit_covid_full)
 
@@ -107,9 +54,32 @@ avg_comparisons(logit_covid_full,
                 newdata = subset(m.data, IdePol == 1),
                 wts = "weights")
 
-## didn't work, again. lets try with cem
+#Testing with extreme-moderate
 
-logit_covid_cem <-glm(COVID ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
-                      data = m.data_cem, family = "binomial"(link = "logit"))
-summary(logit_covid_cem)
+ #NULL model
 
+m.out4 <-matchit(as.factor(ExtMod) ~ EcoCount + Citizen + Age + Sex + NivEco, method =NULL, data = covid,
+                           distance = "glm", link = "logit")
+
+
+## wITH FULL MATCHING
+
+m.out5 <-matchit(as.factor(ExtMod) ~ EcoCount + Citizen + Age + Sex + NivEco, method ="full", data = covid,
+                 distance = "glm", link = "logit")
+
+summary(m.out5)
+
+m.data_ExtMod_full <-match.data(m.out5)
+m.out5
+
+summary(m.out2, un=FALSE)
+plot(summary(m.out2))
+
+logit_full_extMod <-glm(Con ~ ExtMod + Sex + (EcoCount*Age) + Citizen + Educ + NivEco,
+                      data = m.data_ExtMod_full, family = "binomial"(link = "logit"))
+
+summary(logit_full_extMod)
+
+
+logit_covid_full <-glm(Con ~ IdePol + Sex + EcoCount + Citizen + Age + Educ + NivEco,
+                       data = m., family = "binomial"(link = "logit"))
